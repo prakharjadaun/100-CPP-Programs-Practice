f011bf2f2397b5a4dc55033b4f6e2a526ea06433
Prakhar Jadaun <78482329+prakharjadaun@users.noreply.github.com>
2022-07-01 10:31:53 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //fibonacci series 
+    int var=1;
+    int temp=0,res;
+    cout<<"fibonacci series : 0 1";
+    for(int i=0;i<10;i++)
+    {
+        res = temp+var;
+        cout<<"  "<<res;
+        temp = var;
+        var = res;
+    }
+    return 0;
+}
