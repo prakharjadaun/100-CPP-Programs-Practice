58e330024e2a1dc874eae933a5fad1560a32fc3a
Prakhar Jadaun <78482329+prakharjadaun@users.noreply.github.com>
2022-07-02 11:23:26 +0530

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //checking if number is positive or not
+    int n;
+    cout<<"Input a number : ";
+    cin>>n;
+    if(n>0)
+    {
+        cout<<"Number is positive!"<<endl;
+    }
+    else 
+    {
+        cout<<"Number is negative!"<<endl;
+    }
+    return 0;
+}
