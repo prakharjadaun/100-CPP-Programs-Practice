13cd35056c41f2e44e6fb866c6a4a8c1b0fbbd9c
Prakhar Jadaun <78482329+prakharjadaun@users.noreply.github.com>
2022-07-11 00:01:27 +0530

    Update 35.cpp

--- 35.cpp
+++ 35.cpp
@@ -27,5 +27,44 @@ int main()
     std::cout<<isdigit(6)<<std::endl;       //returns 0 as 6 is an integer
     std::cout<<isdigit(ch)<<std::endl;      //retuns 1 as '6' is a digit   
 
+    //5. isgraph()
+    //ch contains 6 
+    std::cout<<isgraph(ch)<<std::endl;      //true condition 
+    ch = '\t';
+    std::cout<<isgraph(ch)<<std::endl;      //false condition 
+    
+    //6. islower()
+    ch = 'i';
+    std::cout<<islower(ch)<<std::endl;      //true condition 
+    std::cout<<islower('I')<<std::endl;     //false condition 
+
+    //7. isprint()
+    ch = 't';
+    std::cout<<isprint(ch)<<std::endl;      //true condition 
+    std::cout<<isprint('\t')<<std::endl;    //false condition 
+
+    //8. ispunct() 
+    std::cout<<ispunct(',')<<std::endl;     //true
+    std::cout<<ispunct('+')<<std::endl;     //true 
+    std::cout<<ispunct('8')<<std::endl;     //false
+
+    //9. isspace()
+    std::cout<<isspace(' ')<<std::endl;      //true 
+    std::cout<<isspace('p')<<std::endl;     //false
+
+    //10 . isupper()
+    ch = 'I';
+    std::cout<<isupper(ch)<<std::endl;      //true condition 
+    std::cout<<isupper('i')<<std::endl;     //false condition 
+
+    //11. isxdigit()
+    ch = '0';
+    std::cout<<isxdigit(ch)<<std::endl;      //true condition
+
+    //12. tolower() 
+    std::cout<<(char)(tolower('T'))<<std::endl;
+
+    //13. toupper()
+    std::cout<<(char)(toupper('t'))<<std::endl;
     return 0;
 }
